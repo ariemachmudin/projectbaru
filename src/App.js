04b3edd28a1c63297e9e24efbe2feffc6269cba5
ariemachmudin <arie.machmudin@gmail.com>
2022-12-09 08:41:57 +0700

    front0812

--- src/App.js
+++ src/App.js
@@ -4,40 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="canvas">
-      <div className="container">
-        <div className='img'>
-          {/* Head */}
-          <img src="images/logo.png" alt="logo" width="10%" height="10%" />
-          {/* Head-end */}
-        </div>
-        <div className='text-welcome'>
-          {/* body */}
-          <p>Selamat Datang Dashboard Admin Manajemen</p>
-          <p>Data Kemenag.AI</p>
-        </div>
-        <div>
-          <p className='text-silahkan'>Silahkan Isi Akun dan Password yang telah terdaftar</p>
-          {/* body-end */}
-        </div>
-        <div className='border'>
-          {/* footer */}
-          <div className='akun'>
-          <a href='akun.html' className='word1'>Akun</a>
-          </div>
-          <div className='password'>
-          <a href='password.html' className='word2'>Password</a>
-          </div>
-        </div>
-        <div className='nav-masuk'>
-          <p className='masuk'>Masuk</p>
-        </div>
-      </div>
+    <div className="sheet">
       <div>
+        <div className='container'>
+          <div className='img'>
+          <img src='images/logo.png' alt='logo' width="10%" height="10%" />
+          </div>
+          <div className='sel'>Selamat Datang Dashboard Admin Manajemen</div>
+          <div className='das'>Data Kemenag.AI</div>
+          <div className='sil'>Silahkan Isi Akun dan Password yang telah terdaftar</div>
+          <div className='ak'>Akun</div>
+          <div className='pass'>Password</div>
+          <div className='mas'>MASUK</div>
+        </div>
         <div className='footer'>
-          <a href="password.html" className='lupa'>Lupa kata sandi?</a>
-          <a href="reset,html" className='reset'>Reset kata sandi</a>
-          {/* footer-end */}
+          <div className='lup'>Lupa Password?</div>
+          <div className='res'>Reset kata sandi</div>
         </div>
 
       </div>
